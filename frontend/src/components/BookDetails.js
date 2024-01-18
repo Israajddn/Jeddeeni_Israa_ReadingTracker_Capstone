1f78c0b25dc5ac55b2bca61ac66f8007fee516bb
Israajddn <Israajddn@gmail.com>
2024-01-18 09:03:22 -0500

    Deleting Data

--- frontend/src/components/BookDetails.js
+++ frontend/src/components/BookDetails.js
@@ -1,17 +1,33 @@
 import React from 'react';
+import { useBooksContext } from '../hooks/useBooksContext';
 
-function BookDetails({book}) {
-    const formatDate = (dateStr) => {
-        const date = new Date(dateStr);
-        return date.toLocaleDateString();
-      }
-      
+function BookDetails({ book }) {
+
+  const { dispatch } = useBooksContext();
+
+  const formatDate = (dateStr) => {
+    const date = new Date(dateStr);
+    return date.toLocaleDateString();
+  }
+
+  const handleClick = async () => {
+    const response = await fetch('/api/readingTracker/' + book._id, {
+      method: 'DELETE'
+    });
+    const json = await response.json();
+
+    if (response.ok) {
+      dispatch({ type: 'DELETE_BOOK', payload: json })
+    }
+  }
+
   return (
     <div className='book-details'>
-        <h4>{book.title}</h4>
-        <p><strong>Author: </strong>{book.author}</p>
-        <p><strong>Date Started: </strong>{formatDate(book.dateStarted)}</p>
-        <p><strong>Date Finished: </strong>{formatDate(book.dateFinished)}</p>
+      <h4>{book.title}</h4>
+      <p><strong>Author: </strong>{book.author}</p>
+      <p><strong>Date Started: </strong>{formatDate(book.dateStarted)}</p>
+      <p><strong>Date Finished: </strong>{formatDate(book.dateFinished)}</p>
+      <span onClick={handleClick}>delete</span>
     </div>
   )
 }
